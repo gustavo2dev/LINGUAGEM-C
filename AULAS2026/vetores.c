198b2d21ccd8925a3e986679c7e58a4b154289f3
Gustavo Iavorski <gustavo2dev@gmail.com>
2026-02-09 19:15:51 -0300

    salva

---
+++ AULAS2026/vetores.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Bom dia mundo !!!");
+    scanf("%i", &num);
+
+    
+    //vetores é abrir mais estaços dentro de uma var;
+
+    int vetor [3];
+    
+
+    return 0;
+}
